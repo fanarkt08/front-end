c6bd25ffa6f2c5b192866773819461aa0e2709df
fanarkt08 <fanarkt08@gmail.com>
2025-07-21 17:00:13 +0200

    activity 8

--- src/pages/Category.jsx
+++ src/pages/Category.jsx
@@ -1,11 +1,17 @@
-import { useParams } from 'react-router';
+import { useParams, useNavigate } from 'react-router';
 
 function Category() {
   const { category } = useParams();
+  const navigate = useNavigate();
+
+  const handleReturn = () => {
+    navigate('/');
+  };
 
   return (
-    <div>
+    <div style={{ padding: '2rem' }}>
       <h1>Catégorie : {category}</h1>
+      <button onClick={handleReturn}>Retour à la page d'accueil</button>
     </div>
   );
 }
