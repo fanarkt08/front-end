e41fc1176426aa529d3446df92a3cae96b162661
fanarkt08 <fanarkt08@gmail.com>
2025-07-21 11:22:06 +0200

    update files

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { Route, Routes } from "react-router";
 import Home from "./pages/Home.jsx";
+import Category from "./pages/Category";
 import Layout from "./components/Layout.jsx";
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
     <Routes>
       <Route element={<Layout/>}>
         <Route path="/" element={<Home />} />
+        <Route path="/categorie/:category" element={<Category />} />
       </Route>
     </Routes>
   );
