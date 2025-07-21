fa0afaf9e615272761e526a4055788ea0bb8095f
fanarkt08 <fanarkt08@gmail.com>
2025-07-21 14:22:13 +0200

    activity 5 main

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,9 @@
-import { Container } from 'react-bootstrap';
-
 export default function Home() {
   return (
-    <Container className="ps-5">
-      <div className="ps-5">
+    <div className="ps-5">
         <div className="ps-5">
-          <h1>Welcome Home !</h1>
+            <h1>Welcome Home !</h1>
         </div>
-      </div>
-    </Container>
+    </div>
   );
 }
