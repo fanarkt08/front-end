77f87a8975efef2fe66160f88188cf655be6a11d
fanarkt08 <fanarkt08@gmail.com>
2025-07-21 12:08:06 +0200

    update 404

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { Route, Routes } from "react-router";
 import Home from "./pages/Home.jsx";
 import Category from "./pages/Category";
+import NotFound from "./pages/NotFound.jsx";
 import Layout from "./components/Layout.jsx";
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
       <Route element={<Layout/>}>
         <Route path="/" element={<Home />} />
         <Route path="/categorie/:category" element={<Category />} />
+        <Route path="*" element={<NotFound />} />
       </Route>
     </Routes>
   );
